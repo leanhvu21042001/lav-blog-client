bb4262eec2e623b78dad551b0f4bcf321d48af88
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-05-20 20:08:18 +0700

    fix: portfolio

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Grid, GridItem, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 
+import { Box, Grid, GridItem, Image, Text, Button, Tooltip } from '@chakra-ui/react';
+
+import { Link } from 'react-router-dom';
 import authorPortfolio from 'src/assets/images/portfolio/author-portfolio.png';
 import facebook from 'src/assets/images/social-media/facebook.svg';
 import github from 'src/assets/images/social-media/github.svg';
@@ -19,43 +21,84 @@ import nodejs from 'src/assets/images/technical/nodejs.svg';
 
 const Portfolio: React.FC = () => {
   return (
-    <Grid templateColumns="3fr 3fr" gap={10}>
-      <GridItem>
-        <Image src={authorPortfolio} />
-      </GridItem>
-      <GridItem fontWeight="bold" mt={5}>
-        <Box fontSize="36px">
-          <Text as="span">Hi, I&apos;m </Text>
-          <Text as="span" color="#BD6161">
-            Raymond
+    <React.Fragment>
+      <Grid templateColumns="3fr 3fr" gap={10}>
+        <GridItem>
+          <Image src={authorPortfolio} />
+        </GridItem>
+        <GridItem fontWeight="bold" mt={5}>
+          <Box fontSize="36px">
+            <Text as="span">Hi, I&apos;m </Text>
+            <Text as="span" color="#BD6161">
+              Raymond
+            </Text>
+          </Box>
+          <Text color="#BD6161" fontSize="26px">
+            Front-End Web Developer
           </Text>
-        </Box>
-        <Text color="#BD6161" fontSize="26px">
-          Front-End Web Developer
-        </Text>
 
-        {/* Social Links */}
-        <Box display="flex" gap={5} mt={10}>
-          <Image src={linkIn} />
-          <Image src={github} />
-          <Image src={facebook} />
-        </Box>
-        {/* Technical */}
+          {/* Social Links */}
+          <Box display="flex" gap={5} mt={10}>
+            <Tooltip label="Go to LinkedIn">
+              <Link to="https://www.linkedin.com/in/lavdev/" target="_blank">
+                <Image src={linkIn} />
+              </Link>
+            </Tooltip>
 
-        <Box display="flex" flexWrap="wrap" gap={5} mt={10}>
-          <Image src={html5} />
-          <Image src={sass} />
-          <Image src={javascript} />
-          <Image src={react} />
-          <Image src={nodejs} />
+            <Tooltip label="Go to Github">
+              <Link to="https://github.com/leanhvu21042001" target="_blank">
+                <Image src={github} />
+              </Link>
+            </Tooltip>
 
-          {/* <Image src={laravel} />
+            <Image src={facebook} />
+          </Box>
+          {/* Technical */}
+
+          <Box display="flex" flexWrap="wrap" gap={5} mt={10}>
+            <Image src={html5} />
+            <Image src={sass} />
+            <Image src={javascript} />
+            <Image src={react} />
+            <Image src={nodejs} />
+
+            {/* <Image src={laravel} />
           <Image src={php} />
           <Image src={bootstrap} />
           <Image src={nestjs} /> */}
-        </Box>
-      </GridItem>
-    </Grid>
+          </Box>
+        </GridItem>
+      </Grid>
+
+      <Tooltip label="Send mail">
+        <Button
+          mt={5}
+          backgroundColor="#BD6161"
+          color="#fff"
+          _hover={{
+            background: 'white',
+            color: 'teal.500',
+          }}
+        >
+          Contact me
+        </Button>
+      </Tooltip>
+
+      <Tooltip label="Download CV">
+        <Button
+          mt={5}
+          ml={2}
+          backgroundColor="#BD6161"
+          color="#fff"
+          _hover={{
+            background: 'white',
+            color: 'teal.500',
+          }}
+        >
+          Curriculum Vitae
+        </Button>
+      </Tooltip>
+    </React.Fragment>
   );
 };
 
